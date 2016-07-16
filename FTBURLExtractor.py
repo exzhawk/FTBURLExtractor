650b35441c7276f015c255622fd472a7ba14815a
Epix Zhang <exzhawk@users.noreply.github.com>
2016-07-17 00:12:49 +0800

    change private pack url

--- FTBURLExtractor.py
+++ FTBURLExtractor.py
@@ -1,11 +1,13 @@
+from xml.etree import ElementTree
+
+import requests
 import requests_cache
 from flask import Flask, render_template
-from xml.etree import ElementTree
-import requests
 
 FTB_URL_PREFIX = 'http://ftb.cursecdn.com/FTB2/'
 STATIC_URL_PREFIX = FTB_URL_PREFIX + 'static/'
 MODPACK_URL_PREFIX = FTB_URL_PREFIX + 'modpacks/'
+PRIVATEPACK_URL_PREFIX = FTB_URL_PREFIX + 'privatepacks/'
 MODPACKS_XML_URL = STATIC_URL_PREFIX + 'modpacks.xml'
 THIRDPARTY_XML_URL = STATIC_URL_PREFIX + 'thirdparty.xml'
 
@@ -13,7 +15,7 @@ app = Flask(__name__)
 requests_cache.install_cache(backend='memory', expire_after=300)
 
 
-def get_modpacks_from_url(xml_url):
+def get_modpacks_from_url(xml_url, private=False):
     r = requests.get(xml_url)
     modpacks_xml = ElementTree.fromstring(r.text)
     modpacks_list = []
@@ -21,8 +23,12 @@ def get_modpacks_from_url(xml_url):
         attrib = modpack.attrib
         if attrib.get('repoVersion'):
             modpacks_list.append(attrib)
+    if private:
+        modpack_url_prefix = PRIVATEPACK_URL_PREFIX
+    else:
+        modpack_url_prefix = MODPACK_URL_PREFIX
     return render_template('modpacks.html', modpacks_list=modpacks_list, static_url_prefix=STATIC_URL_PREFIX,
-                           modpack_url_prefix=MODPACK_URL_PREFIX)
+                           modpack_url_prefix=modpack_url_prefix)
 
 
 @app.route('/')
@@ -42,7 +48,7 @@ def thirdparty():
 
 @app.route('/packcode/<code>')
 def packcode(code):
-    return get_modpacks_from_url(STATIC_URL_PREFIX + code + '.xml')
+    return get_modpacks_from_url(STATIC_URL_PREFIX + code + '.xml', private=True)
 
 
 if __name__ == '__main__':
